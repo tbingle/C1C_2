a572f6beb9ec6db7b61bfa5a1d85fec3b0107269
tbingle <sandmanthomas9@icloud.com>
2025-01-26 10:37:11 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+let products = ["Rose, Sunflower, Lilly, Azalea, Iris"];
+console.log (products);
+products.push ("Tuilp");
+console.log (products);
+products.pop (products);
+console.log (products);
