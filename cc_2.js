2f39042abe837382491c2d06452e98310645e53c
tbingle <sandmanthomas9@icloud.com>
2025-01-26 11:37:43 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -10,4 +10,16 @@ Array[1]= 74;
 console.log (Array);
 let math= (94+83+76+56+33)/5;
 console.log (math);
+let employee = "John Ster";
+let age= 45;
+let department = "Management";
+let role = "isActive";
+console.log("Employee:",employee);
+console.log("Age:",age);
+console.log("Department:",department);
+console.log("Status:",role);
+let departmentupdate = "Senior Management"
+console.log(departmentupdate);
+let posisition = "Senior";
+console.log(posisition);
 
