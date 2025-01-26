24af9c4861fda6f574c0bb6e30b24188876afe0e
tbingle <sandmanthomas9@icloud.com>
2025-01-26 11:21:04 -0500

    Task 2 - Student scores

--- cc_2.js
+++ cc_2.js
@@ -3,4 +3,11 @@ console.log (products);
 products.push ("Tuilp");
 console.log (products);
 products.pop (products);
-console.log (products);
+console.log(products);
+let Array= [94,83,76,56,33];
+console.log (Array);
+Array[1]= 74;
+console.log (Array);
+let math= (94+83+76+56+33)/5;
+console.log (math);
+
